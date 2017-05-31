c1e9048025564c72d8d11a5919506d505d593bf0
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2017-05-30 19:25:50 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var mod=require("./themecrosser.js");
+
 module.exports = function(grunt) {
 	grunt.initConfig({
 		jshint: {
@@ -13,5 +15,24 @@ module.exports = function(grunt) {
 	});	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-bootlint');
+	grunt.registerTask('Mod-function test', 'Tests if the internal mod function is working properly', function() {
+		var inpt="ui-button ui-corner-all ui-widget",
+			out=mod(inpt),
+			expectedOut="ui-button ui-corner-all ui-widget btn rounded",
+			tot=0;
+		grunt.log.writeln(inpt);
+		grunt.log.writeln(out);
+		if (out==expectedOut) return;
+		out=out.split(/[\,\s]/g);
+		expectedOut=expectedOut.split(/[\,\s]/g);
+		for (var i=0;i<out.length;i++) {
+			if (expectedOut.includes(out[i])) {
+				tot++;
+			}
+		}
+		if (tot!=expectedOut.length) {
+			grunt.fail.warn("Output didn't match what was expected",1);//may want to change the error code later
+		}
+	});
 	grunt.registerTask('default', ['jshint','bootlint']);
 };
