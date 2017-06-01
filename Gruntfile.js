017838a7d0a7978a688353abc9c4b83e495fd50d
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2017-06-01 13:00:08 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,5 +42,12 @@ module.exports = function(grunt) {
 			.ok();
 		}
 	});
+	grunt.task.registerTask('foo', 'A sample task that logs stuff.', function(arg1, arg2) {
+  if (arguments.length === 0) {
+    grunt.log.writeln(this.name + ", no args");
+  } else {
+    grunt.log.writeln(this.name + ", " + arg1 + " " + arg2);
+  }
+});
 	grunt.registerTask('default', ['jshint','Mod-function-test','bootlint']);
 };
